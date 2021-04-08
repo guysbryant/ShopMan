da1a04b560dc14c1666c2e82b3ef06fb21d5e533
guysbryant <guy.s.bryant@gmail.com>
2021-04-08 11:16:21 -0400

    crud products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,48 @@
 class ProductsController < ApplicationController
+  before_action :require_logged_in
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :part_number)
+  end
 end
