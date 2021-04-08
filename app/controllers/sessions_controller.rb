18464c37243ebd00fa75c0c33747df9f63cf41c0
guysbryant <guy.s.bryant@gmail.com>
2021-04-08 17:27:58 -0400

    fix user not found when logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to @user
     else
-      redirect_to login_route
+      redirect_to login_url
     end
   end
 
