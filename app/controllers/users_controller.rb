f4a770b048ad2ad5b39784e2e33d578b30e8554a
guysbryant <guy.s.bryant@gmail.com>
2021-04-07 23:34:53 -0400

    crud user and bcrypt has_secure_password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :username, :address, :phone_number, :pay_rate)
+  end
 end
