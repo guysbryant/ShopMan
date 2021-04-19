4f6372067ced2925fa73d1e79a9b8ada14a422f2
guysbryant <guy.s.bryant@gmail.com>
2021-04-19 06:25:07 -0400

    fix customer deletion by adding dependent destroy

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :sales_orders
+  has_many :sales_orders, dependent: :destroy
   has_many :sales_order_lines, through: :sales_orders
   has_many :products, through: :sales_order_lines
 
