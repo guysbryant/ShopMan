6c016b49c922bcabd7ed28639bffcdd050777cc4
Guy Bryant <guy.s.bryant@gmail.com>
2021-04-19 15:55:20 -0400

    add user to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(name: "test", username: "test", password: "test123", address: "123 test", phone_number: 1234567)
 products = [
   {
     name: "Watch",
