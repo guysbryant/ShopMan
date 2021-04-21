6dc0d0fd5191f7652f71cbe9752a5515b49ecc4d
guysbryant <guy.s.bryant@gmail.com>
2021-04-20 21:43:49 -0400

    update index to use new partials directory location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
   helper_method :current_user
 
+  private
+
   def require_logged_in
     redirect_to root_path unless logged_in?
   end
