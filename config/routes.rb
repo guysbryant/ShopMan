9fb1878126ff074b056dd13b3fe5ec3a1f7126b6
guysbryant <guy.s.bryant@gmail.com>
2021-04-21 07:00:51 -0400

    clean up routes; removed commented out code and added except :show to sales order lines resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get "/auth/google_oauth2/callback", to: "sessions#create"
-  resources :sales_order_lines
-  # resources :sessions
-  root to: "sessions#homepage"
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
+  root to: 'sessions#homepage'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
+  resources :sales_order_lines, except: [:show]
   resources :sales_orders do
     resources :sales_order_lines, shallow: true
   end
